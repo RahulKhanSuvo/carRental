cd8e8035ebce4b011b01c0de215f1d9d3b664f35
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-04-21 16:15:16 +0600

    fix: replace HashLoader with FadeLoader in FullScreenSpinner component

--- src/Components/FullScreenSpinner.jsx
+++ src/Components/FullScreenSpinner.jsx
@@ -1,9 +1,9 @@
-import { HashLoader } from "react-spinners";
+import { FadeLoader } from "react-spinners";
 
 const FullScreenSpinner = () => {
   return (
     <div className="fixed inset-0 z-50 bg-white  flex items-center justify-center">
-      <HashLoader color="#FF2C3B" size={60} />
+      <FadeLoader color="#FF2C3B" size={60} />
     </div>
   );
 };
